139e4bd8b93820e44d8cce018aeb6fd9c289bcf9
Miguel <l1f.migue@gmail.com>
2023-04-21 13:58:29 +0100

    asdasdasdsasdadsa

---
+++ pro/2º_trimestre/Agencias de viajes/__main__.py
@@ -0,0 +1,20 @@
+from avion import Avion
+from cliente import Cliente
+from errores import *
+from reserva import Reserva
+from viaje import Viaje
+
+"""
+    Crea un programa que guarde en un fichero de texto, los viajes disponibles para poder vender a sus clientes:
+Los datos a solicitar serán:
+- Origen
+- Destino
+- Billetes Comprados
+- Capacidad Avión
+- Avión (Boeing 747, Boeing 737, Airbus a380)
+- Precio
+
+El programa debe permitir realizar una reserva a nombre de una persona y quedar almacenada esta.
+"""
+
+
---
+++ pro/2º_trimestre/Agencias de viajes/avion.py
@@ -0,0 +1,13 @@
+class Avion():
+    def __init__(self, avion, capacidad):
+        self.avion = avion
+        self.capacidad = capacidad
+        
+    def guardar_avion(self):
+        pass
+    
+    def eliminar_avion(self):
+        pass
+    
+    def datos_avion(self):
+        pass
