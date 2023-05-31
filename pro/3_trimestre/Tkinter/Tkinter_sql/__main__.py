b59557adfe2f76fbab45f5fd25724eb51b190f9b
Miguel <l1f.migue@gmail.com>
2023-05-31 09:15:10 +0100

    base py

---
+++ pro/3_trimestre/Tkinter/Tkinter_sql/__main__.py
@@ -0,0 +1,6 @@
+from bbdd import *
+
+mi_bbdd = BBDD('localhost','root','Miguel36y','CIRCO')
+
+
+mi_bbdd.hacer_consulta('select * from ANIMALES')
--- pro/3_trimestre/Tkinter/Tkinter_sql/bbdd.py
+++ pro/3_trimestre/Tkinter/Tkinter_sql/bbdd.py
@@ -1,3 +1,28 @@
+import mysql.connector
+
 class BBDD():
-    def __init__(self) -> None:
+    def __init__(self,host,user,passwd,bbdd_name):
+        
+        self.BBDD_HOST = host
+        self.BBDD_USER = user
+        self.BBDD_PASS = passwd
+        self.BBDD_BBDD = bbdd_name
+        
+        self.bbdd_conexion = mysql.connector.connect(
+            host    = self.BBDD_HOST
+           ,user    = self.BBDD_USER
+           ,passwd  = self.BBDD_PASS
+           ,db      = self.BBDD_BBDD     
+        )
+        
+        self.cur = self.bbdd_conexion.cursor()
+        
+    def hacer_consulta(self,consulta):
+        self.cur.execute(consulta)
+        for resultado_consulta in self.cur.fetchall():
+            resultado_consulta += resultado_consulta
+        return resultado_consulta
+    
+    def insertar_datos(self,insercion):
         pass
+        
