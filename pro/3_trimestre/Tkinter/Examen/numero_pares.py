9928d79826c565e4ee3c4292a07ca8959bcc35af
Miguel <l1f.migue@gmail.com>
2023-05-31 08:08:07 +0100

    asd

--- pro/3_trimestre/Tkinter/Examen/numero_pares.py
+++ pro/3_trimestre/Tkinter/Examen/numero_pares.py
@@ -4,11 +4,8 @@ for i in range(0,1000):
     lista.append(i)
 
 
-
-
-
 def numeros_pares(lista_numeros):
     return list(filter(lambda x: x%2 == 0,lista_numeros))
 
     
-print(numeros_pares(lista))
+print(numeros_pares(lista))s
