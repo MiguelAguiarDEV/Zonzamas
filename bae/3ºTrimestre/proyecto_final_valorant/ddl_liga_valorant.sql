ea695b89524fd0876cc870d05646d9dcc7ec06e6
Miguel <l1f.migue@gmail.com>
2023-06-05 00:30:52 +0100

    sadasdada

--- bae/3ºTrimestre/proyecto_final_valorant/ddl_liga_valorant.sql
+++ bae/3ºTrimestre/proyecto_final_valorant/ddl_liga_valorant.sql
@@ -4,13 +4,16 @@ CREATE DATABASE liga_valorant;
 
 USE liga_valorant;
 
-DROP TABLE IF EXISTS persona;
-
-DROP TABLE IF EXISTS jugador;
-
+DROP TABLE IF EXISTS personas;
+DROP TABLE IF EXISTS jugadores;
+DROP TABLE IF EXISTS entrenadores;
+DROP TABLE IF EXISTS equipos;
+DROP TABLE IF EXISTS BO3;
+DROP TABLE IF EXISTS partidas;
+DROP TABLE IF EXISTS clasificacion;
+DROP TABLE IF EXISTS mapas;
 
 COMMIT;
-
 
 CREATE TABLE personas (
     cod_persona INT AUTO_INCREMENT,
@@ -22,9 +25,6 @@ CREATE TABLE personas (
     CONSTRAINT pk_persona PRIMARY KEY (cod_persona)
 );
 
-ALTER TABLE personas AUTO_INCREMENT = 0;
-
-
 CREATE TABLE jugadores (
     cod_jugador INT,
     nick VARCHAR(20),
@@ -32,8 +32,7 @@ CREATE TABLE jugadores (
     CONSTRAINT fk_cod_jugador FOREIGN KEY (cod_jugador) REFERENCES personas(cod_persona)
 );
 
-
-CREATE TABLE entrendores (
+CREATE TABLE entrenadores (
     cod_entrenador INT,
     nombre VARCHAR(30),
     CONSTRAINT pk_cod_entrenador PRIMARY KEY (cod_entrenador),
@@ -41,7 +40,7 @@ CREATE TABLE entrendores (
 );
 
 CREATE TABLE equipos (
-    cod_equipo INT,
+    cod_equipo INT AUTO_INCREMENT,
     nombre_equipo VARCHAR(30),
     cod_jugador1 INT default NULL,
     cod_jugador2 INT default NULL,
@@ -53,26 +52,42 @@ CREATE TABLE equipos (
     CONSTRAINT pk_cod_eq PRIMARY KEY (cod_equipo)
 );
 
-
-
-CREATE TABLE partidos (
+CREATE TABLE BO3 (
+    cod_bo3 INT AUTO_INCREMENT,
     cod_equipo_1 INT,
     cod_equipo_2 INT,
     fecha DATE,
-    rondas_equipo1 INT,
-    rondas_equipo2 INT,
-    CONSTRAINT pk_cod_eq1_eq2_fecha PRIMARY KEY (cod_equipo_1,cod_equipo_2,fecha),
-    CONSTRAINT fk_eq1 FOREIGN KEY (cod_equipo_1) REFERENCES equipos(cod_equipo),
-    CONSTRAINT fk_eq2 FOREIGN KEY (cod_equipo_2) REFERENCES equipos(cod_equipo)    
+    CONSTRAINT fk_bo3_eq1 FOREIGN KEY (cod_equipo_1) REFERENCES equipos(cod_equipo),
+    CONSTRAINT fk_bo3_eq2 FOREIGN KEY (cod_equipo_2) REFERENCES equipos(cod_equipo),
+    CONSTRAINT pk_bo3 PRIMARY KEY (cod_bo3)
 );
 
+CREATE TABLE partidas (
+    cod_bo3 INT,
+    cod_equipo_1 INT,
+    cod_equipo_2 INT,
+    num_partida INT,
+    rondas_equipo1 INT,
+    rondas_equipo2 INT,
+    cod_mapa VARCHAR(20),
+    CONSTRAINT fk_par_eq1 FOREIGN KEY (cod_equipo_1) REFERENCES equipos(cod_equipo),
+    CONSTRAINT fk_par_eq2 FOREIGN KEY (cod_equipo_2) REFERENCES equipos(cod_equipo),
+    CONSTRAINT fk_cod_bo3 FOREIGN KEY (cod_bo3) REFERENCES BO3(cod_bo3),    
+    CONSTRAINT pk_cod_bo3_eq1_eq2_num PRIMARY KEY (cod_bo3,num_partida,cod_mapa)
+);
 
 CREATE TABLE clasificacion (
     posicion INT,
     cod_equipo INT,
     nombre_eq VARCHAR(30),
-    CONSTRAINT fk_cod_eq PRIMARY KEY (cod_equipo)
+    CONSTRAINT fk_cod_eq FOREIGN KEY (cod_equipo) REFERENCES equipos(cod_equipo),
+    CONSTRAINT pk_clas_cod_eq PRIMARY KEY (cod_equipo)
+);
+
+CREATE TABLE mapas (
+    cod_mapa INT,
+    partidas_jugadas INT DEFAULT 0,
+    CONSTRAINT pk_mapas PRIMARY KEY (cod_mapa)
 );
 
 COMMIT;
-
--- lnd/3ªTrimestre/DOM/scripts/index.js
+++ lnd/3ªTrimestre/DOM/scripts/index.js
@@ -1,6 +1,50 @@
 //Ejercicio 1//
 let enlaces = document.getElementsByTagName("a");
+console.log(enlaces)
+
 
 let numero_de_enlaces = enlaces.length;
+console.log("Hay",numero_de_enlaces,"enlaces")
 
-let penultimo_enlace = enlaces[2]
+
+
+let penultimo_enlace = enlaces[enlaces.length-2]
+console.log("Señala a esta direccion:",penultimo_enlace.attributes[0])
+
+let j = 0
+for (i = 0; i < enlaces.length ; i++ ){
+    if (enlaces[i].attributes[0].value == "http://prueba/"){
+        j++
+    };
+};
+console.log("Hay",j,"enlaces con la direccion http://prueba/")
+
+
+
+let tercer_parrafo = document.getElementsByTagName("p")[2]
+
+let numero_enlaces_3_parrafo = tercer_parrafo.getElementsByTagName("a").length
+console.log("Numero de enlaces en el tercer parrafo:",numero_enlaces_3_parrafo);
+
+
+let nuevoNodo = document.createElement("div");
+nuevoNodo.textContent = "CIEFP Zonzamas";
+let raiz = document.createElement("div");
+raiz.appendChild(nuevoNodo);
+
+document.getElementById("tabla").appendChild(raiz);
+
+let segundo_parrafo = document.getElementsByTagName("p")[1];
+let nuevo_parrafo = segundo_parrafo.cloneNode(true);
+
+document.getElementsByTagName("p")[2].insertAdjacentElement("beforeend",nuevo_parrafo);
+
+
+
+
+function anadefila(){
+    let fila_anadir = document.createElement("tr")
+    const tabla = document.getElementsByTagName("table")[0]
+    console.log(tabla)
+    tabla.appendChild(fila_anadir)
+};
