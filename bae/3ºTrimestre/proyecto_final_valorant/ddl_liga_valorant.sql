78f73ffc87bdf2a6becaca67d1f02ab244d95021
Miguel <l1f.migue@gmail.com>
2023-06-05 10:47:56 +0100

    asdsadsa

--- bae/3ºTrimestre/proyecto_final_valorant/ddl_liga_valorant.sql
+++ bae/3ºTrimestre/proyecto_final_valorant/ddl_liga_valorant.sql
@@ -4,60 +4,60 @@ CREATE DATABASE liga_valorant;
 
 USE liga_valorant;
 
-DROP TABLE IF EXISTS personas;
-DROP TABLE IF EXISTS jugadores;
-DROP TABLE IF EXISTS entrenadores;
-DROP TABLE IF EXISTS equipos;
-DROP TABLE IF EXISTS BO3;
 DROP TABLE IF EXISTS partidas;
 DROP TABLE IF EXISTS clasificacion;
 DROP TABLE IF EXISTS mapas;
-
-COMMIT;
+DROP TABLE IF EXISTS equipos;
+DROP TABLE IF EXISTS entrenadores;
+DROP TABLE IF EXISTS jugadores;
+DROP TABLE IF EXISTS personas;
+DROP TABLE IF EXISTS BO3;
 
 CREATE TABLE personas (
     cod_persona INT AUTO_INCREMENT,
-    nombre VARCHAR(30),
-    apellido1 VARCHAR(30),
-    apellido2 VARCHAR(30),
-    edad INT,
-    nacionalidad VARCHAR(30),
-    CONSTRAINT pk_persona PRIMARY KEY (cod_persona)
+    nombre VARCHAR(30) NOT NULL,
+    apellido1 VARCHAR(30) NOT NULL,
+    apellido2 VARCHAR(30) NOT NULL,
+    edad INT NOT NULL,
+    nacionalidad VARCHAR(30) NOT NULL,
+    PRIMARY KEY (cod_persona)
 );
 
 CREATE TABLE jugadores (
-    cod_jugador INT,
-    nick VARCHAR(20),
-    CONSTRAINT pk_cod_jugador PRIMARY KEY (cod_jugador),
-    CONSTRAINT fk_cod_jugador FOREIGN KEY (cod_jugador) REFERENCES personas(cod_persona)
+    cod_jugador INT AUTO_INCREMENT,
+    nick VARCHAR(20) UNIQUE NOT NULL,
+    rol VARCHAR(40) CHECK (rol IN ('Iniciador', 'Controlador', 'Duelista', 'Centinela')),
+    cod_persona INT,
+    PRIMARY KEY (cod_jugador),
+    FOREIGN KEY (cod_persona) REFERENCES personas(cod_persona)
 );
-
-ALTER TABLE jugadores ADD CONSTRAINT chk_jugador_entrenador CHECK (cod_jugador NOT IN (SELECT cod_entrenador FROM entrenadores));
-
-
 
 CREATE TABLE entrenadores (
-    cod_entrenador INT,
+    cod_entrenador INT AUTO_INCREMENT,
     nombre VARCHAR(30),
-    CONSTRAINT pk_cod_entrenador PRIMARY KEY (cod_entrenador),
-    CONSTRAINT fk_cod_entrenador FOREIGN KEY (cod_entrenador) REFERENCES personas(cod_persona)    
+    cod_persona INT,
+    PRIMARY KEY (cod_entrenador),
+    FOREIGN KEY (cod_persona) REFERENCES personas(cod_persona) ON DELETE CASCADE
 );
-
-ALTER TABLE entrenadores ADD CONSTRAINT chk_entrenador_jugador CHECK (cod_entrenador NOT IN (SELECT cod_jugador FROM jugadores));
-
-
 
 CREATE TABLE equipos (
     cod_equipo INT AUTO_INCREMENT,
     nombre_equipo VARCHAR(30),
-    cod_jugador1 INT default NULL,
-    cod_jugador2 INT default NULL,
-    cod_jugador3 INT default NULL,
-    cod_jugador4 INT default NULL,
-    cod_jugador5 INT default NULL,
-    cod_suplente6 INT default NULL,
-    cod_entrenador INT default NULL,
-    CONSTRAINT pk_cod_eq PRIMARY KEY (cod_equipo)
+    cod_jugador1 INT DEFAULT NULL,
+    cod_jugador2 INT DEFAULT NULL,
+    cod_jugador3 INT DEFAULT NULL,
+    cod_jugador4 INT DEFAULT NULL,
+    cod_jugador5 INT DEFAULT NULL,
+    cod_jugador6 INT DEFAULT NULL,
+    cod_entrenador INT DEFAULT NULL,
+    PRIMARY KEY (cod_equipo),
+    FOREIGN KEY (cod_jugador1) REFERENCES jugadores(cod_jugador),
+    FOREIGN KEY (cod_jugador2) REFERENCES jugadores(cod_jugador),
+    FOREIGN KEY (cod_jugador3) REFERENCES jugadores(cod_jugador),
+    FOREIGN KEY (cod_jugador4) REFERENCES jugadores(cod_jugador),
+    FOREIGN KEY (cod_jugador5) REFERENCES jugadores(cod_jugador),
+    FOREIGN KEY (cod_jugador6) REFERENCES jugadores(cod_jugador),
+    FOREIGN KEY (cod_entrenador) REFERENCES entrenadores(cod_entrenador)
 );
 
 CREATE TABLE BO3 (
@@ -65,9 +65,9 @@ CREATE TABLE BO3 (
     cod_equipo_1 INT,
     cod_equipo_2 INT,
     fecha DATE,
-    CONSTRAINT fk_bo3_eq1 FOREIGN KEY (cod_equipo_1) REFERENCES equipos(cod_equipo),
-    CONSTRAINT fk_bo3_eq2 FOREIGN KEY (cod_equipo_2) REFERENCES equipos(cod_equipo),
-    CONSTRAINT pk_bo3 PRIMARY KEY (cod_bo3)
+    PRIMARY KEY (cod_bo3),
+    FOREIGN KEY (cod_equipo_1) REFERENCES equipos(cod_equipo),
+    FOREIGN KEY (cod_equipo_2) REFERENCES equipos(cod_equipo)
 );
 
 CREATE TABLE partidas (
@@ -78,24 +78,22 @@ CREATE TABLE partidas (
     rondas_equipo1 INT,
     rondas_equipo2 INT,
     cod_mapa VARCHAR(20),
-    CONSTRAINT fk_par_eq1 FOREIGN KEY (cod_equipo_1) REFERENCES equipos(cod_equipo),
-    CONSTRAINT fk_par_eq2 FOREIGN KEY (cod_equipo_2) REFERENCES equipos(cod_equipo),
-    CONSTRAINT fk_cod_bo3 FOREIGN KEY (cod_bo3) REFERENCES BO3(cod_bo3),    
-    CONSTRAINT pk_cod_bo3_eq1_eq2_num PRIMARY KEY (cod_bo3,num_partida,cod_mapa)
+    PRIMARY KEY (cod_bo3, num_partida, cod_mapa),
+    FOREIGN KEY (cod_bo3) REFERENCES BO3(cod_bo3) ON DELETE CASCADE,
+    FOREIGN KEY (cod_equipo_1) REFERENCES equipos(cod_equipo),
+    FOREIGN KEY (cod_equipo_2) REFERENCES equipos(cod_equipo)
 );
 
 CREATE TABLE clasificacion (
     posicion INT,
     cod_equipo INT,
     nombre_eq VARCHAR(30),
-    CONSTRAINT fk_cod_eq FOREIGN KEY (cod_equipo) REFERENCES equipos(cod_equipo),
-    CONSTRAINT pk_clas_cod_eq PRIMARY KEY (cod_equipo)
+    PRIMARY KEY (cod_equipo),
+    FOREIGN KEY (cod_equipo) REFERENCES equipos(cod_equipo)
 );
 
 CREATE TABLE mapas (
-    cod_mapa INT,
+    cod_mapa INT AUTO_INCREMENT,
     partidas_jugadas INT DEFAULT 0,
-    CONSTRAINT pk_mapas PRIMARY KEY (cod_mapa)
+    PRIMARY KEY (cod_mapa)
 );
-
-COMMIT;
