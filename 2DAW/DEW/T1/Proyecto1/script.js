153ae3fc148cce3ca47c4d46dd5ab64012d8e901
Miguel <l1f.migue@gmail.com>
2023-09-21 22:04:03 +0100

    Tarea DEW

--- 2DAW/DEW/T1/Proyecto1/script.js
+++ 2DAW/DEW/T1/Proyecto1/script.js
@@ -33,9 +33,9 @@ function esColorValido(color) {
 
 
 var numCartelesValido = false;
-
+var numCarteles
 while (numCartelesValido == false){
-  var numCarteles = Number(prompt("Ingrese el numero de carteles"));
+  numCarteles = Number(prompt("Ingrese el numero de carteles"));
   numCartelesValido = numValido(numCarteles, 10);
   if (numCartelesValido == false){
     alert("Numero no valido");
@@ -44,9 +44,9 @@ while (numCartelesValido == false){
 
 
 var coloresSemaforoEsValido = false;  
-
+var colSemaforo
 while (coloresSemaforoEsValido == false){
-  var colSemaforo = prompt("Ingrese el color de la semaforo");
+  colSemaforo = prompt("Ingrese el color de la semaforo");
   coloresSemaforoEsValido = esColorValido(colSemaforo);
   if (coloresSemaforoEsValido == false){
     alert("Color no valido");
@@ -54,66 +54,150 @@ while (coloresSemaforoEsValido == false){
 };
 
 var numCochesValido = false;
-
+var numCoches
 while (numCochesValido == false){
-  var numCoches = Number(prompt("Ingrese el numero de coches"));
+  numCoches = Number(prompt("Ingrese el numero de coches"));
   numCochesValido = numValido(numCoches, 10);
   if (numCochesValido == false){
     alert("Numero no valido");
   }
 };
 
-var numPuertaValido = false;
 
-while (numPuertaValido == false){
-  var numPuerta = Number(prompt("Ingrese el numero de puertas"));
-  numPuertaValido = numValido(numPuerta, 10);
-  if (numPuertaValido == false){
+var numPuertasValido = false;
+var numPuertas;
+while (numPuertasValido == false){
+  numPuertas = Number(prompt("Ingrese el numero de puertas que hay"));
+  numPuertasValido = numValido(numPuertas, 10);
+  if (numPuertasValido == false){
     alert("Numero no valido");
   }
 };
 
 
 
+var numPuertaValido = false;
+var numPuerta;
+while (numPuertaValido == false){
+  numPuerta = Number(prompt("Ingrese el numero de la primera puerta"));
+  numPuertaValido = numValido(numPuerta, 10);
+  if (numPuertaValido == false){
+    alert("Numero no valido");
+  }
+};
+
+var numerosPuertas = [];
+
+for (i=0; i<numPuertas; i++){ 
+  numerosPuertas.push(numPuerta);
+  numPuerta += 2;
+}; 
 
 
+var numEscaparatesValido = false;
+var numEscaparates;
+while (numEscaparatesValido == false){
+  numEscaparates = Number(prompt("Ingrese el numero de escaparates"));
+  numEscaparatesValido = numValido(numEscaparates, 10);
+  console.log("hola")
+  if (numEscaparatesValido == false){
+    alert("Numero no valido");
+  }
+};
 
 
-document.write(cartel.repeat(numCarteles)+"<br>");
+var ofertas = [];
+var oferta;
+var i = 0;
+while (ofertas.length < numEscaparates){
+  oferta = Number(prompt("Ingrese el numero de la oferta del escaparate numero",i));
+  if (numValido(oferta, 100)){
+  ofertas.push(oferta);
+  console.log(oferta)
+  i++;
+  }
+};
 
-if (numPuerta > 0) {
-    document.write("<div id='contenedor'><div>",numPuerta,"</div>",puerta,"</div><br>");        
-}
-else {
-  alert("Numero de puertas no valido");
-  console.log("Numero de puertas no valido");
-}
-document.write(escaparate,"<br>");
 
-document.write(reloj);
-
-
-if (esColorValido(colSemaforo)) {   
-    switch (colSemaforo) {
-      case "verde":
-        document.write(semaforoVerde);
-        break;
-      case "rojo":
-        document.write(semaforoRojo);
-        break;
-      case "amarillo":
-        document.write(semaforoAmarillo);
-        break;
-      default:
-          alert("Color no valido");
-      }
+function verTiempo() {
+    let tiempo = new Date();
+    let hora = tiempo.getHours();
+    let min = tiempo.getMinutes();
+    let sec = tiempo.getSeconds();
+    
+    am_pm = "AM";
+    
+     if (hora >= 12) {
+        if (hora > 12) hora -= 12;
+        am_pm = "PM";
+    } else if (hora == 0) {
+        hr = 12;
+        am_pm = "AM";
     }
-    else {
-      alert("Color no valido");
+ 
+    hora = hora < 10 ? "0" + hora : hora;
+    min = min < 10 ? "0" + min : min;
+    sec = sec < 10 ? "0" + sec : sec;
+ 
+    let tiempoActual = hora + ":" + min + ":" + sec + am_pm;
+    document.getElementById("reloj").innerHTML = tiempoActual;
   };
 
 
 
 
-    document.write("<br>");
+
+
+
+
+document.write(cartel.repeat(numCarteles),"<br>");
+document.write(escaparate.repeat(numEscaparates),"<br>");
+
+
+
+
+document.write("<div>");
+ofertas.forEach(oferta => {
+  document.write("<div class='oferta'>",oferta,"%</div>");
+});
+document.write("</div>");
+
+
+document.write(puerta.repeat(numPuertas),"<br>");
+console.log(numPuertas,"Numero de puertas");
+
+
+
+
+document.write("<div>");
+
+
+numerosPuertas.forEach(puerta => {
+  document.write("<div>",puerta,"</div>");
+});
+document.write("</div>");
+
+
+document.write("<br>");
+
+
+document.write("<div id='contenedor-reloj-semaforo'>");
+document.write("<div id='reloj'></div>")
+setInterval(verTiempo, 1000);
+
+switch (colSemaforo) {
+  case "verde":
+    document.write(semaforoVerde);
+    break;
+  case "rojo":
+    document.write(semaforoRojo);
+    break;
+  case "amarillo":
+    document.write(semaforoAmarillo);
+    break;
+};
+document.write("</div>");
+
+
+document.write("<br>");
 document.write(coche.repeat(numCoches));
