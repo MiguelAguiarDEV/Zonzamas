0744cc731d8df9a04176c9fb1d077465fd4027a0
Miguel <l1f.migue@gmail.com>
2023-09-21 13:58:04 +0100

    Tarea1 DEW

--- 2DAW/DEW/T1/Proyecto1/script.js
+++ 2DAW/DEW/T1/Proyecto1/script.js
@@ -1,71 +1,119 @@
 
+const escaparate = "<img src='img/escaparate.png' alt='escaparate'>";
+const puerta = "<img src='img/puerta.png' alt='puerta'>";
+const cartel = "<img src='img/cartel.png' alt='cartel'>";
+const reloj = "<img src='img/reloj.jpg' alt='reloj'>";
+const coche = "<img src='img/coche.jpg' alt='coche'>";
+const semaforoRojo = "<img src='img/semaforo_rojo.jpg' alt='semaforo'>";
+const semaforoVerde = "<img src='img/semaforo_verde.jpg' alt='semaforo'>";
+const semaforoAmarillo = "<img src='img/semaforo_amarillo.jpg' alt='semaforo'>";
+function numValido(numero, numeroMaximo) {
+    
+  if (!isNaN(numero) && numero >= 0 && numero <= numeroMaximo) {
+      return true;
+  } else {
+      return false;
+  }
+};
+
+const coloresValidos = ["rojo", "verde","amarillo"]; 
+
+function esColorValido(color) {
+    
+
+    if (typeof color === "string") {
+        if (coloresValidos.includes(color.toLocaleLowerCase())) {
+            console.log(color)
+            return true;
+        };  
+    };
+    return false;
+};    
 
 
-const escaparate = "<img src='escaparate.png' alt='escaparate' width='100px'>";
-const puerta = "<img src='puerta.png' alt='puerta' width='100px'>";
-const cartel = "<img src='cartel.png' alt='cartel' width='100px'>";
-const reloj = "<img src='reloj.jpg' alt='reloj' width='100px'>";
-const coche = "<img src='coche.jpg' alt='coche' width='100px'>";
-const semaforoRojo = "<img src='semaforo_rojo.jpg' alt='semaforo' width=100px'>";
-const semaforoVerde = "<img src='semaforo_verde.jpg' alt='semaforo' width=100px'>";
-const semaforoAmarillo = "<img src='semaforo_amarillo.jpg' alt='semaforo' width=100px'>";
+
+var numCartelesValido = false;
+
+while (numCartelesValido == false){
+  var numCarteles = Number(prompt("Ingrese el numero de carteles"));
+  numCartelesValido = numValido(numCarteles, 10);
+  if (numCartelesValido == false){
+    alert("Numero no valido");
+  }
+};
+
+
+var coloresSemaforoEsValido = false;  
+
+while (coloresSemaforoEsValido == false){
+  var colSemaforo = prompt("Ingrese el color de la semaforo");
+  coloresSemaforoEsValido = esColorValido(colSemaforo);
+  if (coloresSemaforoEsValido == false){
+    alert("Color no valido");
+  }
+};
+
+var numCochesValido = false;
+
+while (numCochesValido == false){
+  var numCoches = Number(prompt("Ingrese el numero de coches"));
+  numCochesValido = numValido(numCoches, 10);
+  if (numCochesValido == false){
+    alert("Numero no valido");
+  }
+};
+
+var numPuertaValido = false;
+
+while (numPuertaValido == false){
+  var numPuerta = Number(prompt("Ingrese el numero de puertas"));
+  numPuertaValido = numValido(numPuerta, 10);
+  if (numPuertaValido == false){
+    alert("Numero no valido");
+  }
+};
 
 
 
-var numCarteles ;    
-var colSemaforo ;    
-var numCoches ;    
-var numPuerta ;
 
-numCarteles = prompt("Ingrese el numero de carteles") ;
-colSemaforo = prompt("Ingrese el color de la semaforo") ;
-numCoches = prompt("Ingrese el numero de coches") ;
-numPuerta = prompt("Ingrese el numero de puertas") ;
 
-console.log(colSemaforo)
 
-var coloreSemaforo = ["verde", "rojo", "amarillo"] 
 
-	document.write("<style>");
+document.write(cartel.repeat(numCarteles)+"<br>");
 
-	document.write("* { font-size: 200px;font-weight: bold;}img,font {border:1px solid black;padding:0;margin:0;}img {width:100px;height:100px;}");
-	document.write("</style <br>")
-    document.write(cartel.repeat(numCarteles)+"<br>");
+if (numPuerta > 0) {
+    document.write("<div id='contenedor'><div>",numPuerta,"</div>",puerta,"</div><br>");        
+}
+else {
+  alert("Numero de puertas no valido");
+  console.log("Numero de puertas no valido");
+}
+document.write(escaparate,"<br>");
 
-    if (numPuerta > 0) {
-        document.write("<font size='10px'> ",numPuerta,"</font>",puerta,"<br>");        
+document.write(reloj);
+
+
+if (esColorValido(colSemaforo)) {   
+    switch (colSemaforo) {
+      case "verde":
+        document.write(semaforoVerde);
+        break;
+      case "rojo":
+        document.write(semaforoRojo);
+        break;
+      case "amarillo":
+        document.write(semaforoAmarillo);
+        break;
+      default:
+          alert("Color no valido");
+      }
     }
-
-    document.write(escaparate,"<br>");
-
-    document.write(reloj);
-
-
-    if (typeof colSemaforo !== "string" && coloreSemaforo.includes()) {
-      if (coloreSemaforo.includes(colSemaforo)) {
-        switch (colSemaforo) {
-          case "Verde":
-            document.write(semaforoVerde);
-            break;
-          case "Rojo":
-            document.write(semaforoRojo);
-            break;
-          case "Amarillo":
-            document.write(semaforoAmarillo);
-            break;
-          default:
-              alert("estado no valido");
-          }
-        };
-      }else {
-        alert("Color no valido");
-      };
-
-        document.write("<br>");
-    document.write(coche.repeat(numCoches));
+    else {
+      alert("Color no valido");
+  };
 
 
 
 
-
-  boton.addEventListener("click", generar);
+    document.write("<br>");
+document.write(coche.repeat(numCoches));
