0f55bee701e1bdcf1b3517bc4e7ace370b22d95a
Miguel <l1f.migue@gmail.com>
2023-09-21 23:13:19 +0100

    DSW[PHP]Fibonacci entregado y DEWCalle entreago

---
+++ 2DAW/DSW/T1/lanzarote.lan/index.php
@@ -0,0 +1,85 @@
+<?php
+
+
+
+function gen_fibonacci($lenght){
+	$fibonacci = array();
+	if ($lenght < 2 ){
+		return [0];
+	}
+	elseif ($lenght == 2){
+		return [0,1];
+	}
+	else {
+		$fibonacci[0] = 0;
+        $fibonacci[1] = 1;
+        for ($i=2; $i < $lenght; $i++) { 
+            $fibonacci[$i] = $fibonacci[$i-1] + $fibonacci[$i-2];
+        }
+        return $fibonacci;
+	}
+}
+
+
+function gen_tabla($n_casillas){
+	
+}
+
+
+
+
+
+	
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Mi Página PHP con Estilos</title>
+    <!-- Importa la hoja de estilos CSS -->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <form action="index.php" method="post">
+        <label for="longitud">¿De cuantos numeros quieres que sea la sucesion?</label>
+        <input type="text" name="longitud" id="longitud">
+        <input type="submit" value="Enviar">
+    </form>
+
+            
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $longitud = $_POST["longitud"];
+        
+        $lista_valores = gen_fibonacci($longitud);
+        $tabla = "";
+        
+        $tabla.= "<table>";
+        for ($posicion=0;$posicion < count($lista_valores);$posicion++) {;
+
+            
+            $tabla.= "<tr>";
+
+                for ($i=0;$i<count($lista_valores);$i++) {;
+                    if($i == $posicion){
+                        $tabla.= "<td id='celda-valor'><strong>".$lista_valores[$posicion]."</strong></td>";
+                    }
+                    else{
+                        $tabla.= "<td></td>";
+
+                    };
+                };
+            $tabla.= "</tr>";
+        };
+        $tabla.= "</table>";
+        echo $tabla;
+    }
+
+
+    
+    ?>
+    
+</body>
+</html>
+
