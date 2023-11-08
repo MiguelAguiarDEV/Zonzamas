54c12def271a7dd5a5535cf6fd55fc6fc1009158
Miguel <l1f.migue@gmail.com>
2023-11-08 14:20:07 -0800

    asdsada

---
+++ 2DAW/DEW/T1/teclado/script.js
@@ -0,0 +1,426 @@
+const teclas = {
+    
+    0 : [{
+            "nombre": "º",
+            "normal": "º",
+            "shift": "ª",
+            "altgr": "\\"
+        },
+        {
+            "nombre": "1",
+            "normal": "1",
+            "shift": "!",
+            "altgr": "|"
+        },
+        {
+            "nombre": "2",
+            "normal": "2",
+            "shift": '"',
+            "altgr": "@"
+        },
+        {
+            "nombre": "3",
+            "normal": "3",
+            "shift": "'",
+            "altgr": "#"
+        },
+        {
+            "nombre": "4",
+            "normal": "4",
+            "shift": "$",
+            "altgr": null
+        },
+        {
+            "nombre": "5",
+            "normal": "5",
+            "shift": "%",
+            "altgr": null
+        },
+        {
+            "nombre": "6",
+            "normal": "6",
+            "shift": "&",
+            "altgr": "¬"
+        },
+        {
+            "nombre": "7",
+            "normal": "7",
+            "shift": "/",
+            "altgr": null
+        },
+        {
+            "nombre": "8",
+            "normal": "8",
+            "shift": "(",
+            "altgr": "{"
+        },
+        {
+            "nombre": "9",
+            "normal": "9",
+            "shift": ")",
+            "altgr": "}"
+        },
+        {
+            "nombre": "0",
+            "normal": "0",
+            "shift": "=",
+            "altgr": null
+        },
+        {
+            "nombre": "'",
+            "normal": "'",
+            "shift": "?",
+            "altgr": null
+        },
+        {
+            "nombre": "¡",
+            "normal": "¡",
+            "shift": "¿",
+            "altgr": null
+        },
+        {
+            "nombre": "Retroceso",
+            "normal": "Retroceso",
+            "shift": "Retroceso",
+            "altgr": "Retroceso"
+        }],
+    1 : [{
+            "nombre": "Tab",
+            "normal": "Tab",
+            "shift": null,
+            "altgr": null
+        },
+        {
+            "nombre": "Q",
+            "normal": "q",
+            "shift": "Q",
+            "altgr": null
+        },
+        {
+            "nombre": "W",
+            "normal": "w",
+            "shift": "W",
+            "altgr": null
+        },
+        {
+            "nombre": "E",
+            "normal": "e",
+            "shift": "E",
+            "altgr": "€"
+        },
+        {
+            "nombre": "R",
+            "normal": "r",
+            "shift": "R",
+            "altgr": null
+        },
+        {
+            "nombre": "T",
+            "normal": "t",
+            "shift": "T",
+            "altgr": null
+        },
+        {
+            "nombre": "Y",
+            "normal": "y",
+            "shift": "Y",
+            "altgr": null
+        },
+        {
+            "nombre": "U",
+            "normal": "u",
+            "shift": "U",
+            "altgr": null
+        },
+        {
+            "nombre": "I",
+            "normal": "i",
+            "shift": "I",
+            "altgr": null
+        },
+        {
+            "nombre": "O",
+            "normal": "o",
+            "shift": "O",
+            "altgr": null
+        },
+        {
+            "nombre": "P",
+            "normal": "p",
+            "shift": "P",
+            "altgr": null
+        },
+        {
+            "nombre": "`",
+            "normal": "`",
+            "shift": "^",
+            "altgr": "["
+        },
+        {
+            "nombre": "+",
+            "normal": "+",
+            "shift": "*",
+            "altgr": "]"
+        },
+        {
+            "nombre": "Enter",
+            "normal": "Enter",
+            "shift": "Enter",
+            "altgr": "Enter"
+        }],
+    2 : [{
+            "nombre": "BloqMayus",
+            "normal": "BloqMayús",
+            "shift": "BloqMayús",
+            "altgr": "BloqMayús"
+        },
+        {
+            "nombre": "A",
+            "normal": "a",
+            "shift": "A",
+            "altgr": null
+        },
+        {
+            "nombre": "S",
+            "normal": "s",
+            "shift": "S",
+            "altgr": null
+        },
+        {
+            "nombre": "D",
+            "normal": "d",
+            "shift": "D",
+            "altgr": null
+        },
+        {
+            "nombre": "F",
+            "normal": "f",
+            "shift": "F",
+            "altgr": null
+        },
+        {
+            "nombre": "G",
+            "normal": "g",
+            "shift": "G",
+            "altgr": null
+        },
+        {
+            "nombre": "H",
+            "normal": "h",
+            "shift": "H",
+            "altgr": null
+        },
+        {
+            "nombre": "J",
+            "normal": "j",
+            "shift": "J",
+            "altgr": null
+        },
+        {
+            "nombre": "K",
+            "normal": "k",
+            "shift": "K",
+            "altgr": null
+        },
+        {
+            "nombre": "L",
+            "normal": "l",
+            "shift": "L",
+            "altgr": null
+        },
+        {
+            "nombre": "ñ",
+            "normal": "ñ",
+            "shift": "Ñ",
+            "altgr": null
+        },
+        {
+            "nombre": "´",
+            "normal": "´",
+            "shift": "¨",
+            "altgr": "{"
+        },
+        {
+            "nombre": "ç",
+            "normal": "ç",
+            "shift": "Ç",
+            "altgr": "}"
+        },
+    ],
+    3 : [{
+            "nombre": "Shift",
+            "normal": "Shift",
+            "shift": "Shift",
+            "altgr": "Shift"
+        },
+        {
+            "nombre": "<",
+            "normal": "<",
+            "shift": ">",
+            "altgr": null
+        },
+        {
+            "nombre": "Z",
+            "normal": "z",
+            "shift": "Z",
+            "altgr": null
+        },
+        {
+            "nombre": "X",
+            "normal": "x",
+            "shift": "X",
+            "altgr": null
+        },
+        {
+            "nombre": "C",
+            "normal": "c",
+            "shift": "C",
+            "altgr": null
+        },
+        {
+            "nombre": "V",
+            "normal": "v",
+            "shift": "V",
+            "altgr": null
+        },
+        {
+            "nombre": "B",
+            "normal": "b",
+            "shift": "B",
+            "altgr": null
+        },
+        {
+            "nombre": "N",
+            "normal": "n",
+            "shift": "N",
+            "altgr": null
+        },
+        {
+            "nombre": "M",
+            "normal": "m",
+            "shift": "M",
+            "altgr": null
+        },
+        {
+            "nombre": ",",
+            "normal": ",",
+            "shift": ";",
+            "altgr": null
+        },
+        {
+            "nombre": ".",
+            "normal": ".",
+            "shift": ":",
+            "altgr": null
+        },
+        {
+            "nombre": "-",
+            "normal": "-",
+            "shift": "_",
+            "altgr": null
+        },
+        {
+            "nombre": "Shift",
+            "normal": "Shift",
+            "shift": "Shift",
+            "altgr": "Shift"
+        }
+    ],
+    4 : [
+        {
+            "nombre": "Espacio",
+            "normal": "Espacio",
+            "shift": "Espacio",
+            "altgr": "Espacio"
+        },
+        {
+            "nombre": "AltGr",
+            "normal": "AltGr",
+            "shift": "AltGr",
+            "altgr": "AltGr"
+        },
+    ]
+};
+
+
+
+
+const tecladoHTML = document.getElementById("teclado");
+const texto = document.getElementById("texto");
+bloqMayusActivo = false;
+function crearTecladoHTML (evento){
+    tecladoHTML.innerHTML = "";
+    if (evento === 'normal' || evento === 'shift' || evento === 'altgr') {
+        Object.keys(teclas).forEach((tecla,i) => {
+            const fila = document.createElement("div");
+            fila.classList = ("fila fila" + i);
+
+            teclas[i].forEach((tecla) => {
+                let teclaHTML = document.createElement("div");
+                teclaHTML.innerHTML = tecla[evento];
+                teclaHTML.className = "tecla "+tecla["nombre"];
+                
+                console.log(tecla);
+                fila.appendChild(teclaHTML);
+            });
+
+            tecladoHTML.appendChild(fila);
+        });
+    
+
+        console.log('eventoumento válido: ' + evento);
+    } else {
+        // El eventoumento no es una de las cadenas permitidas
+        console.log('eventoumento no válido: ' + evento);
+    
+    }
+}
+crearTecladoHTML('normal');
+
+
+function anadirtipoTeclados(){
+    tecladoHTML.addEventListener('click', (event) => {
+        if(event.target.classList[0] === 'tecla'){
+            switch (event.target.className) {
+            case 'tecla Shift':
+                tipoTeclado = 'shift';
+                crearTecladoHTML(tipoTeclado);
+                break;
+            case 'tecla AltGr':
+                tipoTeclado = 'altgr';
+                crearTecladoHTML(tipoTeclado);
+                break;
+            case 'tecla BloqMayus':
+                if (bloqMayusActivo) {
+                    bloqMayusActivo = false;
+                    tipoTeclado = 'normal';
+                } else {
+                    bloqMayusActivo = true;
+                    tipoTeclado = 'shift';
+                }
+                crearTecladoHTML(tipoTeclado);
+
+                break;
+            case 'tecla Retroceso':
+                texto.innerHTML = texto.innerHTML.slice(0, -1);
+                break;
+            case 'tecla Enter':
+                texto.innerHTML += "<br>";
+                break;
+            default:
+                if (event.target.className === "tecla Espacio") {
+                    texto.innerHTML += " ";                        
+                }else{
+                    texto.innerHTML += event.target.innerHTML;
+                }
+                if (bloqMayusActivo) {
+                    tipoTeclado = 'shift';
+                }else{
+                    tipoTeclado = 'normal';
+                }
+                crearTecladoHTML(tipoTeclado);
+                break;
+        }}
+    })
+};
+anadirtipoTeclados();
