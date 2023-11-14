58f3c77c333fc5932763b965918725d2ab1966b8
Miguel <l1f.migue@gmail.com>
2023-11-14 13:55:33 +0000

    TECLADO

--- 2DAW/DOR/T1/AngularIntro/intro-angular/src/app/app.component.ts
+++ 2DAW/DOR/T1/AngularIntro/intro-angular/src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { Product } from './models/product.model';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'intro-angular';
+  http = inject(HttpClient);
+  products: Product[] = [];
+  cambiarTitulo() {
+    this.title = 'Titulo cambiado';
+  }
+
+  ngOnInit(){
+    this.http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+    .subscribe((data: any) => {
+      this.products = data;
+
+    })
+  }
 }
--- 2DAW/DOR/T1/AngularIntro/intro-angular/src/app/components/product/product.component.ts
+++ 2DAW/DOR/T1/AngularIntro/intro-angular/src/app/components/product/product.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
+import { Product } from './../../models/product.model';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
 
+export class ProductComponent {
+  @Input() product: Product = {
+    id: 0,
+    title: '',
+    price: 0,
+    images: []
+  };
 }
