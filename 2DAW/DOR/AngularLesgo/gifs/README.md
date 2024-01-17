ec9324b9963f8d77cf57f94856822522e6a1eec2
Miguel Alejandro <miguel.santiesteban.aguiar@gmail.com>
2024-01-17 05:24:22 -0300

    a

--- 2DAW/DOR/AngularLesgo/gifs/README.md
+++ 2DAW/DOR/AngularLesgo/gifs/README.md
@@ -1,6 +1,6 @@
-# GifsApp
+# Gifs
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.3.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.10.
 
 ## Development server
 
--- 2DAW/DOR/AngularLesgo/gifs/src/app/app.component.spec.ts
+++ 2DAW/DOR/AngularLesgo/gifs/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'gifsApp' title`, () => {
+  it(`should have the 'gifs' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('gifsApp');
+    expect(app.title).toEqual('gifs');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gifsApp');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, gifs');
   });
 });
