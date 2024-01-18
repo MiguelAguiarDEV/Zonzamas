7039f5b704968c126140986d25a61b337dd94d95
Miguel <l1f.migue@gmail.com>
2024-01-18 14:03:43 +0000

    arrayPeliculas es un diccionario y da errores

--- 2DAW/DEW/cine_jquery/script.js
+++ 2DAW/DEW/cine_jquery/script.js
@@ -1,292 +1,88 @@
-function numeroRandom (min,max)
-{
+//Si las peliculas estan en local storage las recupera y las guarda en la variable arrayPeliculas.
+if (localStorage.getItem("peliculas")) {
 
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  arrayPeliculas = JSON.parse(localStorage.getItem("movies"));
+  console.log("Peliculas recuoeradas del local storage: " + arrayPeliculas);
 }
 
-
-const cine = document.getElementById('cine');
-cine.innerHTML =    
-`<h1>Cine Atlantida</h1>
-
-<p>Seleccione la pelicula que quiera reservar</p>
-<div id="contenedorPeliculas">
-
-</div>`;
-
-
-
-
-const salas = [];
-
-
-
-
-function crearSala(nombrePelicula, filas, columnas, precioButaca, portada) {
-	// Verificar si las butacas ya están en el localStorage
-    const butacasId = `butacas_${nombrePelicula}`;
-    //
-	const butacasGuardadas = JSON.stringify(localStorage.getItem(butacasId));
-	console.log(butacasGuardadas);
-	
-    let sala;
-	//
-    if (butacasGuardadas === "null") {
-			console.log("Crear aleatorias")
-			  // Si las butacas no están en el localStorage, crear butacas aleatorias
-			  sala = new salaCine(nombrePelicula, filas, columnas, precioButaca, portada,true);
-			  localStorage.setItem(butacasId, JSON.stringify(sala.butacas));
-		
-    } else {
-		// Si las butacas ya están en el localStorage, cargarlas
-		console.log("No crear aleatorias")
-		const butacas = JSON.parse(localStorage.getItem(butacasId));
-		console.log(butacas)
-		sala = new salaCine(nombrePelicula, filas, columnas, precioButaca, portada, false, butacas);
-		
-    }
-    //
-    //Agregar la sala a la array "salas"
-    salas.push(sala);
-    //
-    //console.log('Sala creada ');
-}
-
-
-function mostrarCine(){
-
-	const cine = document.getElementById('cine');
-	cine.innerHTML =    
-	`<h1>Cine Atlantida</h1>
-
-	<div id="contenedorPeliculas">
-
-	</div>`;
-	const contenedorPeliculas = document.getElementById('contenedorPeliculas');
-
-	for (let i=0; i < salas.length; i++){
-		contenedorPeliculas.appendChild(salas[i].mostrarPortada());
-	}
-}
-
-    
-
-    class salaCine
-    {    
-        constructor(nombrePelicula, filas, columnas, precioButaca,portada,generar,butacas)
-        {
-            this.nombrePelicula = nombrePelicula;
-            this.precioButaca = precioButaca;
-            this.filas = filas;
-            this.columnas = columnas;
-            this.butacas = butacas;
-            this.portada = document.createElement("img");
-            this.boton = document.createElement("div");
-            this.generar = generar
-            
-            this.portada.setAttribute("src",portada)
-            
-            if(generar){
-				console.log(generar)
-                this.butacas = []
-                for (let i = 0; i < filas; i++)
-                {
-                    this.butacas[i] = [];
-                    
-                    for (let j = 0; j < columnas; j++)
-                    {
-                        this.butacas[i][j] = numeroRandom(0, 1); // 0 significa libre
-                    }
-                }
-            }
-            
-            console.log('Sala de cine creada');
-            
-        
-        
-        }
-        mostrarButacas() {
-            var self = this;
-            const tablaButacas = document.getElementById("tabla-butacas");
-            const contenedor = document.getElementsByClassName("contenedor-butacas");
-            for (let i = 0; i < self.filas; i++)
-            {
-                const fila = document.createElement("tr");
-                for (let j = 0; j < self.columnas; j++) {
-                    const columna = document.createElement("td");
-                    columna.innerHTML = `<img class='butaca' src='img/sofa.png'>`;
-        
-                    if (self.butacas[i][j] == 1) {
-                        columna.className = "celda libre";
-                        columna.addEventListener("mousedown", function () {
-							columna.className = "celda seleccionada";
-							
-						});
-						  
-						  // Agregar el evento "mouseup" para cuando se suelta el botón del mouse
-						columna.addEventListener("mouseup", function () {
-							self.butacas[i][j] = 2;
-						});
-                        const estado = document.createElement("div");
-                        estado.className = "estado";
-                        estado.innerText = "libre"
-                        columna.addEventListener("mouseover",function () {
-                            columna.appendChild(estado);
-                        })
-
-                        columna.addEventListener("mouseout",function () {
-                            columna.removeChild(estado);
-                        })
-                        
-                    } else {
-                        columna.className = "celda ocupado";
-						//Descomentar si quiere poder liberar los asientos ocupados al clicar.
-                        columna.onclick = function () {
-							
-                            //columna.className = "celda libre";
-                            //self.liberar(i, j);
-                        };
-                    }
-        
-                    fila.appendChild(columna);
-                }
-                tablaButacas.appendChild(fila);
-               
-            }
-            
-            var boton = document.createElement("div");
-            boton.className = "boton-confirmar";
-            boton.innerText = "Reservar";
-            boton.onclick = function () {
-                self.confirmarReserva();
-            };
-            contenedor[0].appendChild(boton);
-        }
-
-        
-
-        confirmarReserva () {
-            var precio = 0;
-            for (let i = 0; i < this.filas; i++)
-            {
-                for (let j = 0; j < this.columnas; j++)
-                {
-                    if (this.butacas[i][j] == 2) {
-                        precio += this.precioButaca;
-                    }
-                }
-            }
-            
-            var a = confirm(`Quiere reservar las butacas seleccionadas?\n
-            Precio total: ` + precio+"$");
-            
-            for (let i = 0; i < this.filas; i++)
-            {
-                for (let j = 0; j < this.columnas; j++)
-                {
-                    if (this.butacas[i][j] == 2) {
-                        if(a){
-                            this.reservar(i, j);
-                        console.log("Reservado");
-                        } else {
-                            this.butacas[i][j] = 1;
-                        }
-                        
-                    } else {
-                        
-                        console.log("No reservado");
-                        
-                    }
-                }
-            }
-            this.mostrarSala();
-        }
-
-
-
-        reservar (fila, columna) {
-            
-            if (this.butacas[fila][columna] == 2) {
-                this.butacas[fila][columna] = 0;
-                this.mostrarSala();
-                
-            } 
-        };
-    
-        // // Método para liberar asientos
-        // liberar (fila, columna) {
-
-        //     if (this.butacas[fila][columna] == 0) 
-        //     {
-        //         this.butacas[fila][columna] = 1; // true significa libre
-        //     }
-        // };
-
-        mostrarPortada() 
-        {
-            var self = this;
-            this.portada.className = "portada";
-            this.portada.onclick = function ()
-            {
-                self.mostrarSala();
-            };
-            return this.portada;
-        };
-
-        mostrarSala() 
-        {
-            cine.innerHTML = ""
-            cine.appendChild(crearBoton("img/flecha-izquierda.png","boton-inicio"));
-            const sala = document.createElement("div");
-            sala.className = "div-sala";
-
-            sala.innerHTML = `
-            <div class="cabecera">
-                <img class="portada-sala" src="${this.portada.getAttribute("src")}">
-                <font class='nombre'>nombre:<font class="pelicula">${this.nombrePelicula}</font></font>
-            </div>
-            <div class = "contenedor-butacas">
-                <table id = "tabla-butacas"></table>
-
-            </div>
-            `
-
-            
-            cine.appendChild(sala);
-            this.mostrarButacas()
-        };
-        
-
-        
+var arrayPeliculas = {
+    "1": {
+        id: 1,
+        nombre: 'Avatar el sentido del agua',
+        director: 'James Cameron',
+        duracion: 120,
+        genero: 'Ciencia Ficcion',
+        urlPortada: 'img/avatar.jpeg',
+        precio: 10,
+        butacas: Array(20).fill(0),
+    },
+    "2": {
+        id: 2,
+        nombre: 'El viaje de Chihiro',
+        director: 'Hayao Miyazaki',
+        duracion: 125,
+        genero: 'Animacion',
+        urlPortada: 'img/chijiro.webp',
+        precio: 5,
+        butacas: Array(23).fill(0),
+    },
+    "3": {
+        id: 3,
+        nombre: 'Interstellar',
+        director: 'Christopher Nolan',
+        duracion: 150,
+        genero: 'Ciencia Ficcion',
+        urlPortada: 'img/Interstellar.jpg',
+        precio: 7,
+        butacas: Array(50).fill(0),
+    },
+    "4": {
+        id: 4,
+        nombre: 'El Hobbit',
+        director: 'Peter Jackson',
+        duracion: 180,
+        genero: 'Fantasia',
+        urlPortada: 'img/hobbit.jpg',
+        precio: 2,
+        butacas: Array(25).fill(0),
+    },
 };
+function generarPortadas(arrayPeliculas) {
+    arrayPeliculas.forEach(pelicula => {
+        $("#cartelera").append(`
+        <section id="portada-${pelicula.nombre.replace(" ", "-")}" class="rounded-9 portada shadow hover-zoom overflow-hidden d-flex justify-content-center">
+			<div class="card" style="width: 18rem;">
+				<img src="${pelicula.urlPortada}" class="card-img-top" alt="...">
+				<div class="card-body">
+					<h5 class="card-title">${pelicula.nombre}</h5>
+                    <p class="card-text">Genero: ${pelicula.genero}</p>
+                    <p class="card-text">Duracuion: ${pelicula.duracion} min</p>  
+                    <button id="btn-${pelicula.id}" type="button" class="btn-reservar btn btn-primary position-relative">
+						Reservar
+						<span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+							${pelicula.precio}€
+						</span>
+                    </button>
+                </div>
+            </div>
+		</section>
+        `)
+    });
+}
+generarPortadas(arrayPeliculas);
 
-
-function crearBoton(ruta,clase){
-        const boton = document.createElement("div");
-    boton.className = clase;
-    let imgAtras = document.createElement("img");
-    imgAtras.setAttribute("src",ruta);
-    
-    boton.onclick = function ()
-    {
-        mostrarCine()
-    }
-    boton.appendChild(imgAtras)
-    return boton
+function generarPaginaReserva(id) {
+    $("#contendor-reserva").append(`
+        <h1 class="text-center">Pelicula: ${arrayPeliculas[id - 1].nombre}</h1>
+    `)
 }
 
 
 
+$(".btn-reservar").click(function () {
+    let id = ($(this).attr("id")).replace("btn-", "");
+     window.location.href = "pagina-reserva.html?id=" + ($(this).attr("id")).replace("btn-", "");
+     generarPaginaReserva(id)
 
+})
 
-
-
-const sala1 =  crearSala('Megalodón', 5, 5, 10, "img/megalodon.jpg");
-const sala2 =  crearSala('La Monja', 15, 10, 5, "img/lamonja.jpg");
-const sala3 =  crearSala('Time', 10, 10, 5, "img/time.jpg");
-const sala4 =  crearSala('El Exorcista', 8, 8, 8, "img/exorcista.jpg");
-const sala5 =  crearSala('Titanic', 12, 12, 6, "img/titanic.jpg");
-
-console.log('-Salas Creadas: ',salas);
-
-mostrarCine() 
