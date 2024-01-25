b49f966099571b7831a331f8e66710e995d4ed34
Miguel Alejandro <miguel.santiesteban.aguiar@gmail.com>
2024-01-25 18:41:13 +0000

    aaa

--- 2DAW/DEW/tree_view/script.js
+++ 2DAW/DEW/tree_view/script.js
@@ -1,16 +1,21 @@
 $(document).ready(function() {
-    if(!$('.btn-exp').data('toggled')){
-        console.log($('.btn-exp').data('toggled'));
-        $('.btn-exp').data('toggled',true)
-        $('.btn-exp').animate({
-            rotate: '90deg'
-        },500);
-    }else{
-        $('.btn-exp').data('toggled',false)
-        $('.btn-exp').animate({
-            rotate: '0deg'
-        },500)  
+
+    function rotar(elemento) {
+        if(!$(elemento).data('toggled')){
+            console.log($(elemento).data('toggled'));
+            $(elemento).data('toggled',true)
+            $(elemento).animate({
+                rotate: '90deg'
+            },500);
+        }else{
+            $(elemento).data('toggled',false)
+            $(elemento).animate({
+                rotate: '0deg'
+            },500)  
+        }
     }
+    rotar('.btn-exp');
+
     $('.btn-add').click(function() {
         // Find the corresponding "hijos" ul
         var hijosList = $(this).closest('.dir').find('.hijos:first');
@@ -22,18 +27,6 @@ $(document).ready(function() {
         // Toggle the visibility of child elements
         $(this).parent().siblings('.hijos').toggle(500);
         // Rotate the ">" button 90 degrees
-        if(!$(this).data('toggled')){
-            console.log($(this).data('toggled'));
-            $(this).data('toggled',true)
-            $(this).animate({
-                rotate: '90deg'
-            },500);
-        }else{
-            $(this).data('toggled',false)
-            $(this).animate({
-                rotate: '0deg'
-            },500)  
-        }
+        rotar(this);
     });
-    
 });
